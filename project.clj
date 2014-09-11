64c3b7c1b906dbea6cb32d1636f9f587a135fcc2
Chris Jenkins <cdpjenkins@gmail.com>
2014-09-11 18:19:03 +0100

    Move servlet api dependency out of dev-dependencies in the hope that it now works on Bluemix...

--- project.clj
+++ project.clj
@@ -2,11 +2,13 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [compojure "1.1.6"]]
+                 [compojure "1.1.6"]
+                 [javax.servlet/servlet-api "2.5"]
+]
   :plugins [[lein-ring "0.8.11"]]
   :ring {:handler hello-clojure.handler/app}
   :profiles
-  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+  {:dev {:dependencies [[ring/ring-jetty-adapter "1.1.0"]
                         [ring-mock "0.1.5"]]}}
   :min-lein-version "2.0.0")
 
